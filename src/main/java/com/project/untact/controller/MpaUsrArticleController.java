d0f15e78019c88d4304b1416496b6272f0f04d63
wooseokoh <allsafe1206@gmail.com>
2021-05-03 23:09:33 +0900

    ResultData를 범용화

--- src/main/java/com/project/untact/controller/MpaUsrArticleController.java
+++ src/main/java/com/project/untact/controller/MpaUsrArticleController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.project.untact.dto.Article;
+import com.project.untact.dto.ResultData;
 import com.project.untact.util.Util;
 
 import lombok.AllArgsConstructor;
@@ -24,24 +26,6 @@ public class MpaUsrArticleController {
 
 		articleLastId = id;
 
-		return new ResultData("S-1", id + "번 글이 작성되었습니다.", article);
+		return new ResultData("S-1", id + "번 글이 작성되었습니다.", "article", article);
 	}
-}
-
-@AllArgsConstructor
-@Data
-class ResultData {
-	private String resultData;
-	private String msg;
-	private Article article;
-}
-
-@AllArgsConstructor
-@Data
-class Article {
-	private int id;
-	private String regDate;
-	private String updateDate;
-	private String title;
-	private String body;
 }
