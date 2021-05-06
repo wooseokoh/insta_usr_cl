8ed95ffcb3a9511337606222a6804f3c6cfeae57
wooseokoh <allsafe1206@gmail.com>
2021-05-06 23:39:57 +0900

    게시물 서비스 도입

--- src/main/java/com/project/untact/controller/MpaUsrArticleController.java
+++ src/main/java/com/project/untact/controller/MpaUsrArticleController.java
@@ -3,12 +3,14 @@ package com.project.untact.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.project.untact.dto.Article;
 import com.project.untact.dto.ResultData;
+import com.project.untact.service.ArticleService;
 import com.project.untact.util.Util;
 
 import lombok.AllArgsConstructor;
@@ -16,14 +18,9 @@ import lombok.Data;
 
 @Controller
 public class MpaUsrArticleController {
-	private List<Article> articles;
-	private int articleLastId;
-
-	public MpaUsrArticleController() {
-		articles = new ArrayList<>();
-		articleLastId = 0;
-		makeTestData();
-	}
+	
+	@Autowired
+	private ArticleService articleService;
 
 	@RequestMapping("/mpaUsr/article/doWrite")
 	@ResponseBody
@@ -32,13 +29,13 @@ public class MpaUsrArticleController {
 		if ( Util.isEmpty(title) ) {
 			return new ResultData("F-1", "제목을 입력해주세요.");
 		}
-
+		
 		if ( Util.isEmpty(body) ) {
 			return new ResultData("F-2", "내용을 입력해주세요.");
 		}
 		
-		int id = writeArticle(title, body);
-		Article article = getArticleById(id);
+		int id = articleService.writeArticle(title, body);
+		Article article = articleService.getArticleById(id);
 
 		return new ResultData("S-1", id + "번 글이 작성되었습니다.", "article", article);
 	}
@@ -46,26 +43,26 @@ public class MpaUsrArticleController {
 	@RequestMapping("/mpaUsr/article/doModify")
 	@ResponseBody
 	public ResultData doModify(Integer id, String title, String body) {
-
+		
 		if ( Util.isEmpty(id) ) {
 			return new ResultData("F-1", "번호를 입력해주세요.");
 		}
-
+		
 		if ( Util.isEmpty(title) ) {
 			return new ResultData("F-2", "제목을 입력해주세요.");
 		}
-
+		
 		if ( Util.isEmpty(body) ) {
 			return new ResultData("F-3", "내용을 입력해주세요.");
 		}
 		
-		boolean modified = modifyArticle(id, title, body);
+		boolean modified = articleService.modifyArticle(id, title, body);
 
 		if (modified == false) {
 			return new ResultData("F-1", id + "번 글이 존재하지 않습니다.", "id", id);
 		}
 
-		return new ResultData("S-1", id + "번 글이 수정되었습니다.", "article", getArticleById(id));
+		return new ResultData("S-1", id + "번 글이 수정되었습니다.", "article", articleService.getArticleById(id));
 	}
 
 	@RequestMapping("/mpaUsr/article/doDelete")
@@ -74,7 +71,8 @@ public class MpaUsrArticleController {
 		if ( Util.isEmpty(id) ) {
 			return new ResultData("F-1", "번호를 입력해주세요.");
 		}
-		boolean deleted = deleteArticleById(id);
+		
+		boolean deleted = articleService.deleteArticleById(id);
 
 		if (deleted == false) {
 			return new ResultData("F-1", id + "번 글이 존재하지 않습니다.", "id", id);
@@ -89,74 +87,13 @@ public class MpaUsrArticleController {
 		if ( Util.isEmpty(id) ) {
 			return new ResultData("F-1", "번호를 입력해주세요.");
 		}
-		Article article = getArticleById(id);
+		
+		Article article = articleService.getArticleById(id);
 
 		if (article == null) {
 			return new ResultData("F-1", id + "번 글은 존재하지 않습니다.", "id", id);
 		}
 
 		return new ResultData("S-1", article.getId() + "번 글 입니다.", "article", article);
-	}
-
-	// 내부
-	private void makeTestData() {
-		for (int i = 0; i < 3; i++) {
-			writeArticle("제목1", "내용1");
-		}
-	}
-
-	private boolean modifyArticle(int id, String title, String body) {
-		Article article = getArticleById(id);
-
-		if (article == null) {
-			return false;
-		}
-
-		article.setUpdateDate(Util.getNowDateStr());
-		article.setTitle(title);
-		article.setBody(body);
-
-		return true;
-	}
-
-	private boolean deleteArticleById(int id) {
-		Article article = getArticleById(id);
-
-		if (article == null) {
-			return false;
-		}
-
-		articles.remove(article);
-		return true;
-	}
-
-	private int writeArticle(String title, String body) {
-		int id = articleLastId + 1;
-		String regDate = Util.getNowDateStr();
-		String updateDate = Util.getNowDateStr();
-
-		Article article = new Article(id, regDate, updateDate, title, body);
-		articles.add(article);
-
-		articleLastId = id;
-
-		return id;
-	}
-
-	private Article getArticleById(int id) {
-		for (Article article : articles) {
-			if (article.getId() == id) {
-				return article;
-			}
-		}
-		
-		/*
-		 * for ( int i = 0; i < articles.size(); i++ ) { Article article =
-		 * articles.get(i);
-		 * 
-		 * if ( article.getId() == id ) { return article; } }
-		 */
-		
-		return null;
 	}
 }
